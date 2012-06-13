61f6123301dd04247c0eb518026f3cf664a726ad
tackman <presitac@gmail.com>
2012-06-13 18:00:34 +0900

    genallスクリプト追加

---
+++ scripts/genall.sh
@@ -0,0 +1,4 @@
+coffee genlist.coffee > ../mylibs/idol_list.coffee
+coffee genids.coffee > ../mylibs/idol_ids.coffee
+coffee genpartof.coffee > ../mylibs/idol_partof.coffee
+cp pre_table.coffee  ../mylibs/idol_table.coffee
