4955b0aefa3b3d19ce4fd784f77997cf20b56690
tackman <presitac@gmail.com>
2012-06-13 18:13:07 +0900

    生成スクリプト改良

--- scripts/genall.sh
+++ scripts/genall.sh
@@ -2,3 +2,10 @@ coffee genlist.coffee > ../mylibs/idol_list.coffee
 coffee genids.coffee > ../mylibs/idol_ids.coffee
 coffee genpartof.coffee > ../mylibs/idol_partof.coffee
 cp pre_table.coffee  ../mylibs/idol_table.coffee
+
+git add ../mylibs/idol_list.coffee
+git add ../mylibs/idol_ids.coffee
+git add ../mylibs/idol_partof.coffee
+git add ../mylibs/idol_table.coffee
+echo "git diffを表示します"
+git diff
